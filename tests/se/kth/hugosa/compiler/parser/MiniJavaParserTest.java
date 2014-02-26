5bbcbe39d88c9f240f611c9f204eb797b5f7be54
Hugo Sandelius <hugo.sandelius@gmail.com>
2014-02-27 00:57:28 +0100

    Created a test file for MiniJavaParser

---
+++ tests/se/kth/hugosa/compiler/parser/MiniJavaParserTest.java
@@ -0,0 +1,25 @@
+package se.kth.hugosa.compiler.parser;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by hugo on 2/27/14.
+ */
+public class MiniJavaParserTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testStart() throws Exception {
+       // throw new Exception();
+    }
+}
