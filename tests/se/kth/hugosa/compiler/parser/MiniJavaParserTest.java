92435dcd0c033c2200bfa8038e1d267e6e5d0d10
Hugo Sandelius <hugo.sandelius@gmail.com>
2014-03-22 17:19:31 +0100

    Added negative tests

--- tests/se/kth/hugosa/compiler/parser/MiniJavaParserTest.java
+++ tests/se/kth/hugosa/compiler/parser/MiniJavaParserTest.java
@@ -1,6 +1,6 @@
 package se.kth.hugosa.compiler.parser;
 
-import java.io.InputStream;
+import java.io.*;
 
 
 import org.junit.After;
@@ -9,8 +9,6 @@ import org.junit.Test;
 import se.kth.hugosa.compiler.Indenter;
 import se.kth.hugosa.compiler.ast.Program;
 
-import java.io.ByteArrayInputStream;
-import java.io.FileInputStream;
 import java.io.InputStream;
 
 public class MiniJavaParserTest {
@@ -29,15 +27,27 @@ public class MiniJavaParserTest {
         InputStream stream = new FileInputStream("tests/minijava/parse_positive.mj");
         MiniJavaParser parser = new MiniJavaParser(stream);
         Program program = parser.parse();
-        System.out.println(Indenter.indent(program.toString()));
+        //System.out.println(Indenter.indent(program.toString()));
     }
-/*
-    @Test(expected = Exception.class)
-    public void negativeTestParse() throws Exception {
-        InputStream stream = new FileInputStream("tests/MiniJavaTest1_negative.mj");
-        MiniJavaParser parser = new MiniJavaParser(stream);
-        Program program = parser.parse();
-        System.out.println(program.toString());
+
+    @Test
+    public void testParseNegative() throws Exception {
+        File dir = new File("tests/minijava/parse_negative");
+        for (File file : dir.listFiles()) {
+            InputStream stream = new FileInputStream(file);
+            MiniJavaParser parser = new MiniJavaParser(stream);
+
+            boolean throwException = false;
+            try {
+                Program program = parser.parse();
+                throwException = true;
+            } catch (Exception e) {
+
+            }
+            if (throwException) {
+                throw new Exception("File: " + file + " was parsed but shouldn't have been.");
+            }
+        }
     }
-*/
+
 }
