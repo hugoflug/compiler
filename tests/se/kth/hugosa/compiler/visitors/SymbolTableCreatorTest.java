2952ed3e4b172cadc64062f428ea78ccc6ffb3ea
Hugo Sandelius <hugo.sandelius@gmail.com>
2014-03-25 19:29:05 +0100

    Added more tests for SymbolTableCreator

--- tests/se/kth/hugosa/compiler/visitors/SymbolTableCreatorTest.java
+++ tests/se/kth/hugosa/compiler/visitors/SymbolTableCreatorTest.java
@@ -6,6 +6,7 @@ import se.kth.hugosa.compiler.ast.Program;
 import se.kth.hugosa.compiler.parser.MiniJavaParser;
 import se.kth.hugosa.compiler.table.ClassTable;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.util.Map;
 
@@ -13,22 +14,41 @@ public class SymbolTableCreatorTest {
 
     @Test
     public void testCreateSymbolTable() throws Exception {
-        MiniJavaParser parser = new MiniJavaParser(new FileInputStream("tests/minijava/symboltable/symboltable.mj"));
-        Program program = parser.parse();
-        SymbolTableCreator creator = new SymbolTableCreator();
-        creator.visit(program);
-        Errors errors = creator.getErrors();
-        if (errors.hasErrors()) {
-            for (Errors.MiniJavaError error : errors.getErrors()) {
-                System.out.println(error);
+        File dir = new File("tests/minijava/symboltable/positive");
+        for (File file : dir.listFiles()) {
+            MiniJavaParser parser = new MiniJavaParser(new FileInputStream(file));
+            Program program = parser.parse();
+            SymbolTableCreator creator = new SymbolTableCreator();
+            creator.visit(program);
+            Errors errors = creator.getErrors();
+            if (errors.hasErrors()) {
+                for (Errors.MiniJavaError error : errors.getErrors()) {
+                    throw new Exception();
+           //         System.out.println(error);
+                }
+            } else {
+                Map<String, ClassTable> classes = creator.getClasses();
+                StringBuilder sb = new StringBuilder();
+                for (Map.Entry<String, ClassTable> entry : classes.entrySet()) {
+                    sb.append(entry.getKey() + " = " + entry.getValue() + "\n");
+                }
+           //     System.out.println(Indenter.indent(sb.toString()));
             }
-        } else {
-            Map<String, ClassTable> classes = creator.getClasses();
-            StringBuilder sb = new StringBuilder();
-            for (Map.Entry<String, ClassTable> entry : classes.entrySet()) {
-                sb.append(entry.getKey() + " = " + entry.getValue() + "\n");
+        }
+    }
+
+    @Test
+    public void testNegativeCreateSymbolTable() throws Exception {
+        File dir = new File("tests/minijava/symboltable/negative");
+        for (File file : dir.listFiles()) {
+            MiniJavaParser parser = new MiniJavaParser(new FileInputStream(file));
+            Program program = parser.parse();
+            SymbolTableCreator creator = new SymbolTableCreator();
+            creator.visit(program);
+            Errors errors = creator.getErrors();
+            if (!errors.hasErrors()) {
+                throw new Exception();
             }
-            System.out.println(Indenter.indent(sb.toString()));
         }
     }
 }
