548e13ec12b04a77f7432d38ff6658f12a9010b4
Hugo Sandelius <hugo.sandelius@gmail.com>
2014-03-26 03:12:41 +0100

    Added typechecking tests

---
+++ tests/se/kth/hugosa/compiler/typechecking/TypeCheckerTest.java
@@ -0,0 +1,36 @@
+package se.kth.hugosa.compiler.typechecking;
+
+import org.junit.Test;
+import se.kth.hugosa.compiler.CompilationException;
+import se.kth.hugosa.compiler.Errors;
+import se.kth.hugosa.compiler.ast.Program;
+import se.kth.hugosa.compiler.parser.MiniJavaParser;
+import se.kth.hugosa.compiler.symboltable.ClassTable;
+import se.kth.hugosa.compiler.symboltable.SymbolTableCreator;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Map;
+
+public class TypeCheckerTest {
+    @Test
+    public void testTypeCheck() throws Exception {
+        File dir = new File("tests/minijava/typecheck/positive");
+        for (File file : dir.listFiles()) {
+            MiniJavaParser parser = new MiniJavaParser(new FileInputStream(file));
+            Program program = parser.parse();
+            SymbolTableCreator creator = new SymbolTableCreator();
+            Map<String, ClassTable> classes = null;
+            try {
+                classes = creator.createSymbolTable(program);
+            } catch (CompilationException e) {
+                for (Errors.MiniJavaError error : e.getErrors().getErrors()) {
+                    throw new Exception();
+                }
+            }
+
+            TypeChecker typeChecker = new TypeChecker(classes);
+            typeChecker.visit(program);
+        }
+    }
+}
