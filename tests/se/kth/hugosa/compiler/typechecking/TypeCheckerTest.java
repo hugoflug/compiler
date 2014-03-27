e4b1787c2e48e47dce1bc0c57d5a7a47ff2cc206
Hugo Sandelius <hugo.sandelius@gmail.com>
2014-03-27 13:50:01 +0100

    Added negative TypeChecker tests

--- tests/se/kth/hugosa/compiler/typechecking/TypeCheckerTest.java
+++ tests/se/kth/hugosa/compiler/typechecking/TypeCheckerTest.java
@@ -1,6 +1,9 @@
 package se.kth.hugosa.compiler.typechecking;
 
+import org.junit.Assert;
 import org.junit.Test;
+import se.kth.hugosa.compiler.CompilationException;
+import se.kth.hugosa.compiler.Indenter;
 import se.kth.hugosa.compiler.ast.Program;
 import se.kth.hugosa.compiler.parser.MiniJavaParser;
 import se.kth.hugosa.compiler.symboltable.ClassTable;
@@ -24,6 +27,27 @@ public class TypeCheckerTest {
 
             TypeChecker typeChecker = new TypeChecker(classes);
             typeChecker.typeCheck(program);
+        }
+    }
+
+    @Test
+    public void testTypeCheckNegative() throws Exception {
+        File dir = new File("tests/minijava/typecheck/negative");
+        for (File file : dir.listFiles()) {
+            System.out.println(file);
+            MiniJavaParser parser = new MiniJavaParser(new FileInputStream(file));
+            Program program = parser.parse();
+            SymbolTableCreator creator = new SymbolTableCreator();
+            Map<String, ClassTable> classes = null;
+            classes = creator.createSymbolTable(program);
+
+            try {
+                TypeChecker typeChecker = new TypeChecker(classes);
+                typeChecker.typeCheck(program);
+                Assert.fail("Should have thrown CompilationException");
+            } catch (CompilationException e) {
+
+            }
         }
     }
 }
