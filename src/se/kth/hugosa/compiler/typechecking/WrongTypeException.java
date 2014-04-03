24a317b370a46923180b12521d8bcdebc28f8eb5
Hugo Sandelius <hugo.sandelius@gmail.com>
2014-04-03 22:01:37 +0200

    WrongTypeException can now take multiple accepted types

--- src/se/kth/hugosa/compiler/typechecking/WrongTypeException.java
+++ src/se/kth/hugosa/compiler/typechecking/WrongTypeException.java
@@ -3,15 +3,25 @@ package se.kth.hugosa.compiler.typechecking;
 import se.kth.hugosa.compiler.CompilationException;
 import se.kth.hugosa.compiler.ast.Type;
 
+import java.util.List;
+
 public class WrongTypeException extends CompilationException {
     private Type type;
     private String var;
     private Type shouldBe;
     private int line, column;
+    private List<Type> acceptedTypes;
 
     public WrongTypeException(Type type, Type shouldBe, int line, int column) {
         this.type = type;
         this.shouldBe = shouldBe;
+        this.var = null;
+        this.line = line;
+    }
+
+    public WrongTypeException(Type type, List<Type> acceptedTypes, int line, int column) {
+        this.type = type;
+        this.acceptedTypes = acceptedTypes;
         this.var = null;
         this.line = line;
     }
@@ -29,6 +39,13 @@ public class WrongTypeException extends CompilationException {
         sb.append("WrongTypeException (line " + line + ", column " + column + "): ");
         if (var == null) {
             sb.append("Actual type: " + type.toPrettyString() + ". " + shouldBe.toPrettyString() + " expected");
+        } else if (acceptedTypes != null) {
+            sb.append("Actual type: " + type.toPrettyString() + ". One of: ");
+            for (Type type : acceptedTypes) {
+                sb.append(type + ", ");
+            }
+            sb.substring(sb.length() - 1);
+            sb.append("expected");
         } else {
             sb.append(var + "is of type: " + type.toPrettyString() + ". " + shouldBe.toPrettyString() + " expected");
         }
